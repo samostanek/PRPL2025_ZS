bb6cf9e70953a00585a1d9a7932235f3db5a517b
Samuel Stanek <samostanek@gmail.com>
2025-12-03 15:53:53 +0100

    feat: update fast spectrometry for 8 detectors

--- golem_data_loader/golem_data_loader.py
+++ golem_data_loader/golem_data_loader.py
@@ -66,10 +66,14 @@ class DataValidationError(DataLoadError):
 class SpectroscopyLine(Enum):
     """Enumeration of available fast spectrometry signals."""
 
-    H_ALPHA = ("Hα", "U_656.csv")
-    H_BETA = ("C III + O II", "U_467.csv")
-    HE_I = ("He I", "U_589.csv")
-    WHOLE = ("Whole", "U_whole.csv")
+    H_ALPHA = ("Hα", "DAS_raw_data_dir/ch8.csv")
+    Cl_II = ("Cl II 479.5nm", "DAS_raw_data_dir/ch5.csv")
+    HE_I = ("He I 588nm", "DAS_raw_data_dir/ch7.csv")
+    WHOLE = ("Whole", "DAS_raw_data_dir/ch6.csv")
+    C_II = ("C II 514.5nm", "DAS_raw_data_dir/ch4.csv")
+    N_II = ("N II 568.6nm", "DAS_raw_data_dir/ch3.csv")
+    O_I = ("O I 777nm", "DAS_raw_data_dir/ch2.csv")
+    He_I = ("He I 447.1nm", "DAS_raw_data_dir/ch1.csv")
 
     def __init__(self, display_name: str, filename: str):
         self.display_name = display_name
